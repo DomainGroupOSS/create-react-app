93e4b3b205913a9b760b9103cc5d7b5ecc1383a3
Albert Still <albertjamesstill@gmail.com>
2018-02-13 12:21:28 +1100

    remove SPA boilerplate

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -37,10 +37,9 @@ module.exports = function(
 
   // Setup the script rules
   appPackage.scripts = {
-    start: 'react-scripts start',
-    build: 'react-scripts build',
-    test: 'react-scripts test --env=jsdom',
-    eject: 'react-scripts eject',
+    start: 'domain-react-scripts start',
+    build: 'domain-react-scripts build',
+    test: 'domain-react-scripts test --env=jsdom',
   };
 
   fs.writeFileSync(
